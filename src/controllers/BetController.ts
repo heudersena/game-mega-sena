f3d3f7f58d48098d264e7600051e751d90d439b2
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-05-23 17:41:02 -0300

    adicionando 1 se não existe nenhum jogo iniciado

--- src/controllers/BetController.ts
+++ src/controllers/BetController.ts
@@ -7,10 +7,10 @@ class BetController {
         const { numbers, establishmentId } = request.body;
 
         const game = await prisma.game.findFirst({ orderBy: { created_at: "desc" } })
-        const qty = game?.match_id! + 1
+        const qty = game?.match_id! ? game?.match_id! + 1 : 1
 
         try {
-            
+
             const old_number = await prisma.bet.findFirst({ orderBy: { created_at: "desc" } })
             const number_insert = Boolean(old_number?.namber_bet) == true ? old_number?.namber_bet! + 1 : 1
             const content = await prisma.bet.create({
