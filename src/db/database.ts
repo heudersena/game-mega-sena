794f8780b32ece586f03c542b499e5a1a9b22d1a
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-05-25 17:35:40 -0300

    removendo log querys database

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 // { log: ["query", "info", "error", "warn"] }
-const prisma = new PrismaClient({ log: ["query"] }) // log: ["query"]
+const prisma = new PrismaClient() // log: ["query"]
 
 export { prisma }
 
